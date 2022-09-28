a4abbec1920c025905bb9a275440caf929384d33
Jesus Jo <jesusjo@MacBook-Pro-de-X.local>
2022-09-28 13:59:55 -0700

    project structure, componentes created

---
+++ src/components/index.jsx
@@ -0,0 +1,24 @@
+import Navbar from './Navbar';
+import Billing from './Billing';
+import Business from './Business';
+import CardDeal from './CardDeal';
+import Client from './Client';
+import CTA from './CTA';
+import Stats from './Stats';
+import Footer from './Footer';
+import Testimonials from './Testimonials';
+import Hero from './Hero';
+
+
+export { 
+    Navbar,
+    Billing,
+    CardDeal, 
+    Business, 
+    Client, 
+    CTA, 
+    Stats, 
+    Footer, 
+    Testimonials,
+    Hero
+};
